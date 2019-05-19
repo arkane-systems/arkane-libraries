c667a084c145e2ec3f38283cfa0b076dd68f3903
Alistair Young <avatar@arkane-systems.net>
2019-05-19 14:14:16 -0500

    Fixed configuration for Sphinx 2

--- docs/conf.py
+++ docs/conf.py
@@ -21,6 +21,8 @@ project = 'Arkane Libraries'
 copyright = '2019, Alistair J. R. Young'
 author = 'Alistair J. R. Young'
 
+master_doc = 'contents'
+
 # The full version, including alpha/beta/rc tags
 release = 'pre'
 
@@ -47,7 +49,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
