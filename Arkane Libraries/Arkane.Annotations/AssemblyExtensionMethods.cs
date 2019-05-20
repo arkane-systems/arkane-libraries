38fba2724677a8da660972a0ecb846b2c0bf53c3
Alistair Young <avatar@arkane-systems.net>
2019-05-20 01:41:33 -0500

    Extension methods framework.

---
+++ Arkane Libraries/Arkane.Annotations/AssemblyExtensionMethods.cs
@@ -0,0 +1,33 @@
+﻿#region header
+
+// Arkane.Annotations - AssemblyExtensionMethods.cs
+// 
+// Alistair J. R. Young
+// Arkane Systems
+// 
+// Copyright Arkane Systems 2012-2019.  All rights reserved.
+// 
+// Created: 2019-05-19 8:01 PM
+
+#endregion
+
+#region using
+
+using JetBrains.Annotations ;
+
+#endregion
+
+// ReSharper disable once CheckNamespace
+namespace System.Reflection
+{
+    /// <summary>
+    ///     Extension methods for reading <see cref="Assembly" /> attributes.
+    /// </summary>
+    [PublicAPI]
+
+    // ReSharper disable once InconsistentNaming
+#pragma warning disable IDE1006 // Naming Styles
+    public static class αο
+#pragma warning restore IDE1006 // Naming Styles
+    { }
+}
