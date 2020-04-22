092f26bb482ab9de599facc4ce00e5f15443aec1
Alistair Young <avatar@arkane-systems.net>
2020-04-22 15:27:31 -0500

    Add presence switch to internal.

---
+++ Arkane Libraries/Arkane.Core/Internal/ModuleInit.cs
@@ -0,0 +1,42 @@
+﻿#region header
+
+// Arkane.Core - ModuleInit.cs
+// 
+// Alistair J. R. Young
+// Arkane Systems
+// 
+// Copyright Arkane Systems 2012-2020.  All rights reserved.
+// 
+// Created: 2020-04-22 3:26 PM
+
+#endregion
+
+#region using
+
+using System ;
+
+using JetBrains.Annotations ;
+
+using PostSharp.Aspects ;
+
+#endregion
+
+namespace ArkaneSystems.Arkane.Internal
+{
+    /// <summary>
+    ///     Module initializer. Marked static method runs whenever this module is initialized.
+    /// </summary>
+    public static class ModuleInit
+    {
+        /// <summary>
+        ///     Module initializer. Runs whenever this module is initialized.
+        /// </summary>
+        [ModuleInitializer (0)]
+        [UsedImplicitly]
+        public static void InitializeModule ()
+        {
+            // Mark our presence in the AppContext.
+            AppContext.SetSwitch ("Switch.ArkaneSystems.Arkane.Core.Presence", true) ;
+        }
+    }
+}
