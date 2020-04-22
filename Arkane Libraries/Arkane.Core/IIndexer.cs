9bc57baa1734f651876e884b9b3e621e5f8ab496
Alistair Young <avatar@arkane-systems.net>
2020-04-22 14:56:37 -0500

    IIndexer.

---
+++ Arkane Libraries/Arkane.Core/IIndexer.cs
@@ -0,0 +1,35 @@
+﻿#region header
+
+// Arkane.Core - IIndexer.cs
+// 
+// Alistair J. R. Young
+// Arkane Systems
+// 
+// Copyright Arkane Systems 2012-2019.  All rights reserved.
+// 
+// Created: 2020-04-22 2:25 PM
+
+#endregion
+
+namespace ArkaneSystems.Arkane
+{
+    /// <summary>
+    ///     An interface defining a type with a key-based indexer.
+    /// </summary>
+    /// <typeparam name="TKey">The key of the desired value.</typeparam>
+    /// <typeparam name="TValue">The value corresponding to the key.</typeparam>
+    public interface IIndexer <in TKey, TValue>
+    {
+        TValue this [TKey key] { get ; set ; }
+    }
+
+    /// <summary>
+    ///     An interface defining a type with a read-only key-based indexer.
+    /// </summary>
+    /// <typeparam name="TKey">The key of the desired value.</typeparam>
+    /// <typeparam name="TValue">The value corresponding to the key.</typeparam>
+    public interface IReadOnlyIndexer <in TKey, out TValue>
+    {
+        TValue this [TKey key] { get ; }
+    }
+}
