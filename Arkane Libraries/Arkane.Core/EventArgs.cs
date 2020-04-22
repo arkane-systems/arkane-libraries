a458725a895f718e264ac1ec685741bf89a85dc4
Alistair Young <avatar@arkane-systems.net>
2020-04-22 15:35:02 -0500

    EventArgs

---
+++ Arkane Libraries/Arkane.Core/EventArgs.cs
@@ -0,0 +1,42 @@
+﻿#region header
+
+// Arkane.Core - EventArgs.cs
+// 
+// Alistair J. R. Young
+// Arkane Systems
+// 
+// Copyright Arkane Systems 2012-2020.  All rights reserved.
+// 
+// Created: 2020-04-22 3:22 PM
+
+#endregion
+
+#region using
+
+using System ;
+
+using JetBrains.Annotations ;
+
+#endregion
+
+namespace ArkaneSystems.Arkane
+{
+    /// <summary>
+    ///     Generic event argument structure holding a single value.
+    /// </summary>
+    /// <typeparam name="T">The type of the contained value.</typeparam>
+    [PublicAPI]
+    public class EventArgs <T> : EventArgs
+    {
+        /// <summary>
+        ///     Initializes a new instance of the ArkaneSystems.Arkane.Base.EventArgs&lt;T&gt; class.
+        /// </summary>
+        /// <param name="value">The value of the event argument.</param>
+        public EventArgs (T value) => this.Value = value ;
+
+        /// <summary>
+        ///     The typed value contained in the EventArgs{T}.
+        /// </summary>
+        public T Value { get ; }
+    }
+}
